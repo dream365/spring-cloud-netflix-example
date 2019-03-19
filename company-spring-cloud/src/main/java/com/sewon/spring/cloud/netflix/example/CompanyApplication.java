55da1021948f106e1681ea8c2fad291563e4a28f
Sewon Ko <sewon.ko@ybrain.com>
2019-03-19 14:19:02 +0900

    add eureka server & enable discovery client

--- company-spring-cloud/src/main/java/com/sewon/spring/cloud/netflix/example/CompanyApplication.java
+++ company-spring-cloud/src/main/java/com/sewon/spring/cloud/netflix/example/CompanyApplication.java
@@ -3,9 +3,11 @@ package com.sewon.spring.cloud.netflix.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 public class CompanyApplication {
